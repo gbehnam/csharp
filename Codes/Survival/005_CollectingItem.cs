c1ca164ae12bd9adcb3002f4d4a363319b851395
Behnam <gbehnam.bb@hotmail.com>
2020-07-27 15:59:12 +0200

    Create 005_CollectingItem.cs

---
+++ Codes/Survival/005_CollectingItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    // type: "void onTriggerEnter" and press enter
+    private void OnTriggerEnter(Collider other) // in the setting isTrigger value has been set to true
+    {
+        //the object(colliding with this.gameObject/ PlayerToMove) needs to be tagged Player otherwise nothing would happen
+        if (other.tag == "Player" & other.name == "PlayerToMove") 
+        {
+            // before destroying the object, on can:
+            // add count(poits)
+            // change variables and ...
+            Destroy(this.gameObject);
+        }
+        
+    }
+
+    //if the isTrigger is not set:
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+    */
+}

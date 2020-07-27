28b89146a0fa86776a4b75c9b1f8ec184412e320
Behnam <gbehnam.bb@hotmail.com>
2020-07-27 15:58:36 +0200

    Create 004_UpDownLoopMove.cs

---
+++ Codes/Survival/004_UpDownLoopMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDownMove : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * _speed, 10), transform.position.z);
+    }
+}

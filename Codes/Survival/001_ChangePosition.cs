ccdba606ccb53e5d0e2f781894b6ae7e474800c0
Behnam <gbehnam.bb@hotmail.com>
2020-07-27 10:50:42 +0200

    Create 001_ChangePosition.cs

---
+++ Codes/Survival/001_ChangePosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Vector3 startPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //startPosition = new Vector3(0, 0, 0);
+        // grab the previous position = (Assign) a new position
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

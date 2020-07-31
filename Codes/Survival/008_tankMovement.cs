9ab1fc5c2cfd76f0d9db72eb56173fae027cce8c
Behnam <gbehnam.bb@hotmail.com>
2020-07-31 17:57:44 +0200

    Create 008_tankMovement.cs

---
+++ Codes/Survival/008_tankMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tankMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontalInput, transform.position.y, verticalInput) * 5 * Time.deltaTime);
+        transform.Rotate(new Vector3(transform.rotation.x, horizontalInput, transform.rotation.z));
+
+    }
+}
